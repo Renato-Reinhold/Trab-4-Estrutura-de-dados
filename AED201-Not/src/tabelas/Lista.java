fee29f51d594616e0292e13a231c53d2c20dfccb
Renato Muller Reinhold <42810260+Renato-Reinhold@users.noreply.github.com>
2020-06-11 19:45:20 -0300

    Trab-4

---
+++ AED201-Not/src/tabelas/Lista.java
@@ -0,0 +1,18 @@
+package tabelas;
+
+public class Lista<K, T>{
+
+	K chave;
+	T valor;
+	
+	public boolean inserir(K chave, T valor) {
+		return false;
+	}
+	
+	public T remover(K chave) {
+		return null;
+	}
+	
+	
+	
+}
---
+++ AED201-Not/src/tabelas/MapaDispersao.java
@@ -0,0 +1,32 @@
+package tabelas;
+
+public class MapaDispersao<K, T> {
+
+	
+	private Lista<K, T>[] tabela;
+	
+	public MapaDispersao(int quantidade) {
+
+	}
+	
+	private int caucularHash(K chave) {
+		return 0;
+	}
+	
+	public boolean inserir(K chave, T dado) {
+		return false;
+	}
+	
+	public T remover(K chave) {
+		return null;
+	}
+	
+	public T buscar (K chave) {
+		return null;
+	}
+	
+	public int quantosElementos() {
+		return 0;
+	}
+	
+}
