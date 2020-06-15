dcc907c4c2587ba983278fde086ff008e9e4df55
Renato Muller Reinhold <42810260+Renato-Reinhold@users.noreply.github.com>
2020-06-15 11:08:33 -0300

    parte do veiculo

--- AED201-Not/src/tabelas/Lista.java
+++ AED201-Not/src/tabelas/Lista.java
@@ -32,6 +32,6 @@ public class Lista<K, T>{
 	
 	@Override
 	public String toString() {
-		return "["+chave.toString()+"] = " + dado + ((prox != null) ? " | " + prox.toString() : "\n");
+		return "\n["+chave.toString()+"] = " + dado + ((prox != null) ? " | " + prox.toString() : "\n");
 	}
 }
